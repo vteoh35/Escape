6ff852532e2674e17bca121914d4be1375823bc4
mov360 <146847389+mov360@users.noreply.github.com>
2026-08-25 00:02:35 +1000

    DB connection established and bug fix

--- backend/src/API/program.cs
+++ backend/src/API/program.cs
@@ -1,8 +1,13 @@
 using API.Tasks;
 using Application.Tasks;
 using Infrastructure.Tasks;
+using Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddSingleton<ITaskRepository, TaskRepository>();
 builder.Services.AddScoped<CreateTask>();
